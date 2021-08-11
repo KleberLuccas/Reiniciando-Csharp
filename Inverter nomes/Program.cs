71192bfec823c65f3d8c7994583b4503345c9296
KleberLuccas <kleberluccas062@gmail.com>
2021-08-11 04:31:00 -0300

    Inverter nomes

--- Inverter nomes/Program.cs
+++ Inverter nomes/Program.cs
@@ -6,7 +6,32 @@ namespace Inverter_nomes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string primeiroNome, segundoNome, terceiroNome, quartoNome, auxiliar;
+
+            Console.WriteLine("Digite o primeiro nome");
+            primeiroNome = Console.ReadLine();
+            Console.WriteLine("Digite o segundo nome");
+            segundoNome = Console.ReadLine();
+            Console.WriteLine("Digite o terceiro nome");
+            terceiroNome = Console.ReadLine();
+            Console.WriteLine("Digite o quarto nome");
+            quartoNome = Console.ReadLine();
+
+            //Mecanismo para inverter os nomes
+            auxiliar = primeiroNome;
+            primeiroNome = quartoNome;
+            quartoNome = auxiliar;
+            auxiliar = segundoNome;
+            segundoNome = terceiroNome;
+            terceiroNome = auxiliar;
+
+            Console.WriteLine();
+            Console.WriteLine("Nomes inseridos em ordem invertida");
+            Console.WriteLine(primeiroNome);
+            Console.WriteLine(segundoNome);
+            Console.WriteLine(terceiroNome);
+            Console.WriteLine(quartoNome);
+            Console.ReadKey();
         }
     }
 }
