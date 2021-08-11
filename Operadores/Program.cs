bad2eeb21f42778b99dc74d142632efeb1573de7
KleberLuccas <kleberluccas062@gmail.com>
2021-08-11 11:19:31 -0300

    update

---
+++ Operadores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region operações básicas
+            /*
+            int num1 = 10;
+            int num2 = 2;
+            //int resultado = (num1 + num2)
+           
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 % num2);
+
+            Console.WriteLine(num1 + num2 - num1 * num2)
+            */
+            #endregion
+        }
+    }
+}
